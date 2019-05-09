b5a7adc0d1af1efe56867336b90e22922625ca4b
Justin Steenhuis <justinsteenhuis@gmail.com>
2019-05-09 13:25:53 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/plot-viewer/precache-manifest.74484639a3e74da340c14b480b387a0d.js"
+  "/plot-viewer/precache-manifest.eae1e907794bd509ec948ff573771ce1.js"
 );
 
 workbox.clientsClaim();
